b388eb49850b1bed390c548951d0bcd2a48512a9
s.morenom <s.morenom@uniandes.edu.co>
2019-08-21 22:42:00 -0500

    actualizacion controlador

--- src/controller/ControladorLista.java
+++ src/controller/ControladorLista.java
@@ -51,12 +51,24 @@ throw new Exception("Los comandos no están organizados de la forma adecuada");
 				actual = actual.darSiguiente();
 			}
 			reader2 = new CSVReader(new FileReader("./data/bogota-cadastral-2018-2-All-MonthlyAggregate.csv"));
-			comandos= reader.readNext();  //La primera línea no se debe leer
+			comandos= reader2.readNext();  //La primera línea no se debe leer
 			if(comandos.length!=7||!comandos[0].equals("sourceid")||!comandos[1].equals("dstid")||!comandos[2].equals("month")||!comandos[3].equals("mean_travel_time")||!comandos[4].equals("standard_deviation_travel_time")||!comandos[5].equals("geometric_mean_travel_time")||!comandos[6].equals("geometric_standard_deviation_travel_time"))
 				throw new Exception("Los comandos no están organizados de la forma adecuada");
 			Viaje actual2= ultimo;
 			for(String[] nextLine : reader) {
 				Viaje i = new Viaje(Integer.parseInt(nextLine[0]),Integer.parseInt(nextLine[1]),Integer.parseInt(nextLine[2]),Double.parseDouble(nextLine[3]),Double.parseDouble(nextLine[4]),Double.parseDouble(nextLine[5]),Double.parseDouble(nextLine[6]), null);
+				if(i.darMes()==01)
+					enero++;
+				else if(i.darMes()==02)
+					febrero++;
+				else if(i.darMes()==03)
+					marzo++;
+				else if(i.darMes()==04)
+					abril++;
+				else if(i.darMes()==05)
+					mayo++;
+				else if(i.darMes()==06)
+					junio++;
 				actual.darSiguiente();
 				actual = actual.darSiguiente();
 			}
